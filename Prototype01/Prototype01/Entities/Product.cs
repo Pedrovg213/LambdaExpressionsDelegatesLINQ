296985f9908cedd7ce08ee7fd88f8873a673dc8d
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-27 11:11:45 -0300

    Lambda Functions Introduction

---
+++ Prototype01/Prototype01/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Prototype01.Entities {
+   internal class Product {
+
+      public string Name {
+         get; set;
+      }
+      public double Price {
+         get; set;
+      }
+
+
+      public Product( string _name , double _price ) {
+
+         Name = _name;
+         Price = _price;
+
+      }
+
+
+      public override string ToString( ) {
+
+         return (
+            $"{Name}, " +
+            $"{Price.ToString( "F2" , CultureInfo.InvariantCulture )}" );
+
+      }
+   }
+}
---
+++ Prototype01/Prototype01/Program.cs
@@ -0,0 +1,26 @@
+﻿using Prototype01.Entities;
+
+namespace Prototype01 {
+   internal class Program {
+      static void Main( string[ ] args ) {
+
+         List<Product> products = new List<Product>();
+
+         products.Add( new Product( "TV" , 900.00 ) );
+         products.Add( new Product( "Notebook" , 1200.00 ) );
+         products.Add( new Product( "Tablet" , 450.00 ) );
+
+         // Lambda
+         products.Sort( ( p1 , p2 ) => p1.Name.ToUpper().CompareTo( p2.Name.ToUpper() ) );
+
+         foreach ( Product p in products )
+            Console.WriteLine( p );
+
+      }
+      static int CompareProducts( Product _product01 , Product _product02 ) {
+
+         return ( _product01.Name.ToUpper().CompareTo( _product02.Name.ToUpper() ) );
+
+      }
+   }
+}
