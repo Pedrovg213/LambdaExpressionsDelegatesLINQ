22a16c63bbb7e81388acecfb89c94d49a313b269
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-27 11:27:37 -0300

    Delegate introduction

---
+++ Prototype02/Prototype02/Program.cs
@@ -0,0 +1,23 @@
+﻿using Prototype02.Service;
+
+namespace Prototype02 {
+
+   // delegate
+   delegate double BinaryNumericOperation( double _n1 , double _n2 );
+
+   internal class Program {
+      static void Main( string[ ] args ) {
+
+         double a = 10;
+         double b = 12;
+
+         // delegate
+         BinaryNumericOperation sum = CalculationService.Sum;
+         BinaryNumericOperation max = new BinaryNumericOperation(CalculationService.Max);
+
+         Console.WriteLine( sum.Invoke( a , b ) );
+         Console.WriteLine( max( a , b ) );
+
+      }
+   }
+}
---
+++ Prototype02/Prototype02/Service/CalculationService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Prototype02.Service {
+   internal class CalculationService {
+
+      public static double Max( double _x , double _y ) {
+
+         return ( _x > _y ) ? _x : _y;
+
+      }
+      public static double Sum( double _x , double _y ) {
+
+         return ( _x + _y );
+
+      }
+      public static double Square( double _x ) {
+
+         return ( _x * _x );
+
+      }
+   }
+}
