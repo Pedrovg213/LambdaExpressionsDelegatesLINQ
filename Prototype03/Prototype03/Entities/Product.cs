a0ec300ab5c59e5acda9b89abfe01069454e460e
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-27 12:09:08 -0300

    Predicate Introduction

---
+++ Prototype03/Prototype03/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Prototype03.Entities {
+   internal class Product {
+
+      public string Name {
+         get; set;
+      }
+      public double Price {
+         get; set;
+      }
+
+
+      public Product( string _name , double _price ) {
+
+         Name = _name;
+         Price = _price;
+
+      }
+
+
+      public override string ToString( ) {
+         return $"{Name}, {Price.ToString( "F2" , CultureInfo.InvariantCulture )}";
+      }
+   }
+}
---
+++ Prototype03/Prototype03/Program.cs
@@ -0,0 +1,27 @@
+﻿using Prototype03.Entities;
+
+namespace Prototype03 {
+   internal class Program {
+      static void Main( string[ ] args ) {
+
+         List<Product> products = new List<Product>();
+
+         products.Add( new Product( "Tv" , 900.00 ) );
+         products.Add( new Product( "Mouse" , 50.00 ) );
+         products.Add( new Product( "Tablet" , 350.00 ) );
+         products.Add( new Product( "HD Case" , 80.90 ) );
+
+         // predicate
+         products.RemoveAll( ProductTest );
+         foreach ( Product pro in products )
+            Console.WriteLine( pro );
+
+      }
+
+      static bool ProductTest( Product _product ) {
+
+         return ( _product.Price >= 100 );
+
+      }
+   }
+}
