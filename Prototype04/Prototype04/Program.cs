8a41acca2462cdcf8ad3855e00be796459f2d0c6
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-28 16:52:42 -0300

    Linq Introduction

---
+++ Prototype04/Prototype04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Prototype04 {
+   internal class Program {
+      static void Main( string[ ] args ) {
+
+         // Specify the data source
+         int[] numbers = new int[] { 2, 3, 4, 5, 6 };
+
+         // Define the query expression
+         IEnumerable<int> result = numbers.
+            Where(i => i % 2 == 0).
+            Select(i => i * 10);
+
+         // Execute the query
+         foreach ( int i in result )
+            Console.WriteLine( i );
+
+      }
+   }
+}
