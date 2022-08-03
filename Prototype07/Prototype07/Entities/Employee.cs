a7d1fbae1aaebbe35a2100d5eb7c00d5fe826f91
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-08-03 15:31:28 -0300

    Lambda Expressions Exercise
    - Prototype07: finished.

---
+++ Prototype07/Prototype07/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Prototype07.Entities {
+	internal class Employee {
+
+		public string Name {
+			get; set;
+		}
+		public string Email {
+			get; set;
+		}
+		public double Salary {
+			get; set;
+		}
+
+
+		public Employee( string _name , string _email , double _salary ) {
+
+			Name = _name;
+			Email = _email;
+			Salary = _salary;
+
+		}
+	}
+}
--- Prototype07/Prototype07/Program.cs
+++ Prototype07/Prototype07/Program.cs
@@ -1,7 +1,48 @@
-﻿namespace Prototype07 {
+﻿using System.Globalization;
+using Prototype07.Entities;
+
+namespace Prototype07 {
 	internal class Program {
 		static void Main( string[ ] args ) {
+
+			// Inputs
+			// file path
+			Console.WriteLine( "Enter full file path:" );
+			string filePath = Console.ReadLine();
+			//C:\Users\pedro\Desktop\C# Estudo\Seção 17 - Espressões lambda, delegates, LINQ\LambdaExpressionsDelegatesLINQ\Prototype07\Prototype07\Files\Input File.txt
 			
+			// cut-off salary
+			Console.Write( "Enter the cut-off salary value: " );
+			double cutSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			Console.WriteLine();
+
+			// Processing
+			List<Employee> employees = new List<Employee>();
+
+			using ( StreamReader stream = File.OpenText( filePath ) ) {
+				while ( !stream.EndOfStream ) {
+
+					string[] infos = stream.ReadLine().Split(',');
+					string name = infos[0];
+					string email = infos[1];
+					double salary = double.Parse(infos[2], CultureInfo.InvariantCulture);
+
+					employees.Add( new Employee( name , email , salary ) );
+				}
+			}
+
+			// Outputs
+			Console.WriteLine( $"Email of people whose salary is more than ${cutSalary.ToString( "F2" , CultureInfo.InvariantCulture )}:" );
+			IEnumerable<string> emails = employees.Where( e => e.Salary > cutSalary ).OrderBy( e => e.Email ).Select(e => e.Email);
+
+			foreach ( string eml in emails )
+				Console.WriteLine( eml );
+			Console.WriteLine();
+
+			double sumWithM = employees.Where(e => e.Name[0] == 'M').Sum(p => p.Salary);
+			Console.Write( $"Sum of salary of people whose name starts with 'M': ${sumWithM.ToString( "F2" , CultureInfo.InvariantCulture )}" );
+			Console.WriteLine();
+
 		}
 	}
 }
